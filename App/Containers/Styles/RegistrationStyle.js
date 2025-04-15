9c68ab2c67fc5a472f6f78860151415f397e0310
parveen <Venkatesh63740@gmail.com>
2025-04-15 11:42:46 +0530

    initial commit.

---
+++ App/Containers/Styles/RegistrationStyle.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native'
+import { width, height } from 'react-native-dimension'
+
+export default StyleSheet.create({
+  container: {
+    /* paddingVertical: 20,
+    borderTopWidth: 1,
+    borderColor: "#CED0CE",
+    width: width,
+    height: 300,
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    paddingTop: (height / 2) - 50 */
+  }
+})
